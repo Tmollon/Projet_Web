0c6374ba17d0fc336845de47edbe59acb695dd83
Tmollon <tom.mollon.69@gmail.com>
2021-12-03 00:15:29 +0100

    inscription to PDO

--- php/Controller.php
+++ php/Controller.php
@@ -117,7 +117,6 @@ function Contacter()
             } elseif (empty($message)) {
                 echo "Le champ message est vide.";
             } else {
-
                 $sql = 'INSERT INTO Avis (NomPrenom,Email,Avis) VALUES (:nom,:email,:avis)';
                 $sth = $conn->prepare($sql);
                 $sth->execute(array(':nom' => $nom, ':email' => $email, ':avis' => $message));
@@ -137,39 +136,43 @@ function Contacter()
 
 function Inscrire()
 {
-    $BDD = array();
-    $BDD['host'] = "localhost";
-    $BDD['user'] = "root";
-    $BDD['pass'] = "password";
-    $BDD['db'] = "drugs";
-    $mysqli = mysqli_connect($BDD['host'], $BDD['user'], $BDD['pass'], $BDD['db']);
-    if (!$mysqli) {
-        echo "Connexion non établie.";
-        exit;
-    }
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
 
-    $AfficherFormulaire = 1;
-    if (isset($_POST['pseudo'], $_POST['email'], $_POST['mdp'])) {
-        if (empty($_POST['pseudo'])) {
-            echo "Le champ Pseudo est vide.";
-        } elseif (!preg_match("#^[a-z0-9]+$#", $_POST['pseudo'])) {
-            echo "Le Pseudo doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
-        } elseif (strlen($_POST['pseudo']) > 25) {
-            echo "Le pseudo est trop long, il dépasse 25 caractères.";
-        } elseif (empty($_POST['mdp'])) {
-            echo "Le champ Mot de passe est vide.";
-        } elseif (mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM membres WHERE pseudo='" . $_POST['pseudo'] . "'")) == 1) {
-            echo "Ce pseudo est déjà utilisé.";
-        } else {
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=drugs", $username, $password);
 
-            if (!mysqli_query($mysqli, "INSERT INTO membres SET pseudo='" . $_POST['pseudo'] . "',email='" . $_POST['email'] . "', mdp='" . password_hash($_POST['mdp'], PASSWORD_DEFAULT) . "'")) {
-                echo "Une erreur s'est produite: " . mysqli_error($mysqli);
+
+        $AfficherFormulaire = 1;
+
+        $stmt = $conn->prepare("SELECT * FROM membres WHERE pseudo = ?");
+        $stmt->execute([$_POST['pseudo']]);
+        $user = $stmt->fetch();
+
+        if (isset($_POST['pseudo'], $_POST['email'], $_POST['mdp'])) {
+            if (empty($_POST['pseudo'])) {
+                echo "Le champ Pseudo est vide.";
+            } elseif (!preg_match("#^[a-z0-9]+$#", $_POST['pseudo'])) {
+                echo "Le Pseudo doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
+            } elseif (strlen($_POST['pseudo']) > 25) {
+                echo "Le pseudo est trop long, il dépasse 25 caractères.";
+            } elseif (empty($_POST['mdp'])) {
+                echo "Le champ Mot de passe est vide.";
+            } elseif ($user) {
+                echo "Ce pseudo est déjà utilisé.";
             } else {
-                echo "Vous êtes inscrit avec succès!";
 
+                $sql = 'INSERT INTO membres (pseudo,mdp,email) VALUES (:pseudo,:mdp,:email)';
+                $sth = $conn->prepare($sql);
+                $sth->execute(array(':pseudo' => $_POST['pseudo'], ':mdp' => password_hash($_POST['mdp'], PASSWORD_DEFAULT), ':email' => $_POST['email']));
+                require('inscri.php');
                 $AfficherFormulaire = 0;
             }
         }
+    } catch (PDOException $e) {
+        $conn->rollBack();
+        echo "Erreur : " . $e->getMessage();
     }
     if ($AfficherFormulaire == 1) {
         require('inscription.php');
---
+++ php/inscri.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="Styles/traitement.css">
+
+<div id="message_remerciement">
+    <p>Vôtre compte est crée drogue toi à fond</p>
+</div>
