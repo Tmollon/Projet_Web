1df7034c2641023e40cef43e39d2290c86dcf6ac
Tmollon <tom.mollon.69@gmail.com>
2021-12-02 21:37:42 +0100

    change PDO contact

--- php/Controller.php
+++ php/Controller.php
@@ -41,7 +41,6 @@ function traitement()
 function connexion()
 {
     require("connexion.php");
-    
 }
 
 function deconnexion()
@@ -54,7 +53,8 @@ function deconnexion()
 }
 
 
-function seconnecter(){
+function seconnecter()
+{
 
     if (isset($_POST['pseudo']) && isset($_POST['mdp'])) {
         $db_username = 'root';
@@ -69,74 +69,78 @@ function seconnecter(){
 
         $username = mysqli_real_escape_string($db, htmlspecialchars($_POST['pseudo']));
         $password = mysqli_real_escape_string($db, htmlspecialchars($_POST['mdp']));
-    
+
         if ($username !== "" && $password !== "") {
             $requete = "SELECT count(*) FROM membres where 
                   pseudo = '" . $username . "' and mdp = '" . md5($password) . "' ";
             $exec_requete = mysqli_query($db, $requete);
             $reponse      = mysqli_fetch_array($exec_requete);
             $count = $reponse['count(*)'];
-            if ($count != 0) 
-            {
+            if ($count != 0) {
                 $_SESSION['pseudo'] = $username;
                 header('Location: index.php?action=Accueil');
             } else {
-                header('Location: connexion.php?erreur=1'); 
+                header('Location: connexion.php?erreur=1');
             }
         } else {
-            header('Location: connexion.php?erreur=2'); 
+            header('Location: connexion.php?erreur=2');
         }
     }
-    mysqli_close($db); 
+    mysqli_close($db);
 
 
     var_dump($_SESSION['pseudo']);
     require('Accueil.php');
-
 }
 
 function Contacter()
 {
 
-    $BDD = array();
-    $BDD['host'] = "localhost";
-    $BDD['user'] = "root";
-    $BDD['pass'] = "password";
-    $BDD['db'] = "drugs";
-    $mysqli = mysqli_connect($BDD['host'], $BDD['user'], $BDD['pass'], $BDD['db']);
-    if (!$mysqli) {
-        echo "Connexion non établie.";
-        exit;
-    }
 
-    
-    $AfficherFormulaire = 1;
-    
-    if (isset($_POST['nom'], $_POST['Email'], $_POST['Message'])) {
-    
-        if (empty($_POST['nom'])) {
-            echo "Le champ nom est vide.";
-        } elseif (empty($_POST['Email'])) {
-            echo "Le champ Email est vide.";
-        } elseif (empty($_POST['Message'])) {
-            echo "Le champ message est vide.";
-        } else {
-    
-            if (!mysqli_query($mysqli, "INSERT INTO Avis SET NomPrenom='" . $_POST['nom'] . "', Email='" . $_POST['Email'] . "', Avis='" . $_POST['Message'] . "'")) {
-                echo "Une erreur s'est produite: " . mysqli_error($mysqli);
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+
+    try {
+
+        $conn = new PDO("mysql:host=$servername;dbname=drugs", $username, $password);
+        echo ('salut');
+        $AfficherFormulaire = 1;
+
+        if (isset($_POST['nom'], $_POST['Email'], $_POST['Message'])) {
+
+            $nom = $_POST['nom'];
+            $email = $_POST['Email'];
+            $message = $_POST['Message'];
+            var_dump($email);
+
+            if (empty($nom)) {
+                echo "Le champ nom est vide.";
+            } elseif (empty($email)) {
+                echo "Le champ Email est vide.";
+            } elseif (empty($message)) {
+                echo "Le champ message est vide.";
             } else {
+
+                $sql = 'INSERT INTO Avis (NomPrenom,Email,Avis) VALUES (:nom,:email,:avis)';
+                $sth = $conn->prepare($sql);
+                $sth->execute(array(':nom' => $nom, ':email' => $email, ':avis' => $message));
                 require('traitement.php');
-                $AfficherFormulaire = 0;
+                $AfficherFormulaire == 0;
             }
         }
+        if ($AfficherFormulaire == 1) {
+            require('Contact.php');
+        }
+    } catch (PDOException $e) {
+        $conn->rollBack();
+        echo "Erreur : " . $e->getMessage();
     }
-    if ($AfficherFormulaire == 1) {
-        require('Contact.php');
-    }
-    
 }
 
-function Inscrire(){
+
+function Inscrire()
+{
     $BDD = array();
     $BDD['host'] = "localhost";
     $BDD['user'] = "root";
@@ -147,7 +151,7 @@ function Inscrire(){
         echo "Connexion non établie.";
         exit;
     }
-    
+
     $AfficherFormulaire = 1;
     if (isset($_POST['pseudo'], $_POST['email'], $_POST['mdp'])) {
         if (empty($_POST['pseudo'])) {
@@ -161,12 +165,12 @@ function Inscrire(){
         } elseif (mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM membres WHERE pseudo='" . $_POST['pseudo'] . "'")) == 1) {
             echo "Ce pseudo est déjà utilisé.";
         } else {
-    
+
             if (!mysqli_query($mysqli, "INSERT INTO membres SET pseudo='" . $_POST['pseudo'] . "',email='" . $_POST['email'] . "', mdp='" . md5($_POST['mdp']) . "'")) {
                 echo "Une erreur s'est produite: " . mysqli_error($mysqli);
             } else {
                 echo "Vous êtes inscrit avec succès!";
-    
+
                 $AfficherFormulaire = 0;
             }
         }
@@ -174,6 +178,4 @@ function Inscrire(){
     if ($AfficherFormulaire == 1) {
         require('inscription.php');
     }
-
 }
-?>
