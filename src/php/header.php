463b949d5b2091234c69f692ef3e943472498f71
Kyllian HKY. Holder <holderk@ginkoia.fr>
2021-12-05 14:23:38 +0100

    menu change

--- src/php/header.php
+++ src/php/header.php
@@ -1,19 +1,19 @@
 <header>M4_Dr2g$.c0m </header>
 
 <nav id="menu">
-    <ul id="menu-closed">
+ 
 
         <li><a href="index.php?action=Accueil">Accueil</a></li>
         <li><a href="index.php?action=Douce">Douce</a></li>
         <li><a href="index.php?action=Dure">Dure</a></li>
         <li><a href="index.php?action=Legal">Legale</a></li>
         <li><a href="index.php?action=Contact">Contact</a></li>
+        <li></li>
+        <li></li>
+      
 
-        <li><a href="#menu-closed">&#215; </a></li>
-        <li><a href="#menu">&#9776; </a></li>
 
-
-    </ul>
+    
 
 </nav>
 
